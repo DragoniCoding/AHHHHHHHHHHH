da9e2fb51e3e2da77da9854b28e5263885cfeeeb
DragoniCoding <126823174+DragoniCoding@users.noreply.github.com>
2023-03-06 15:25:27 -0600

    edited program, updated index

--- CScode/Arrayal/sketch.js
+++ CScode/Arrayal/sketch.js
@@ -1,15 +1,49 @@
-// Project Title
-// Your Name
+// Array Demo
+// Aidan J. Thorpe
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let shapes = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  spawnBall(width/2, height/2);
 }
 
 function draw() {
   background(220);
+  moveBalls();
+  displayBalls();
+}
+
+function mousePressed() {
+  spawnBall(mouseX, mouseY);
+}
+
+function displayBalls() {
+  for (let i=0; i<shapes.length; i++) {
+    fill(shapes[i].colour);
+    circle(shapes[i].x, shapes[i].y, shapes[i].diam);
+  }
+}
+
+function moveBalls() {
+  for (let i=0; i<shapes.length; i++) {
+    shapes[i].x += shapes[i].dx;
+    shapes[i].y += shapes[i].dy;
+  }
+}
+
+function spawnBall(tempX, tempY) {
+  let newBall = {
+    x: tempX,
+    y: tempY,
+    dx: random(-5, 5),
+    dy: random(-10, 10),
+    diam: random(30, 125),
+    colour: color(random(255), random(255), random(255)),
+  };
+  shapes.push(newBall);
 }
--- index.md
+++ index.md
@@ -1,6 +1,7 @@
 # AHH CS30 2023
 
 ## emos
-- [movinRectangle](CScode/movinRectangle)
+- [movinRectangle](CScode/movinRectangle/sketch.js)
+- [LoopingLeaps](CScode/LoopingLeaps/sketch.js)
 
 ## Prejects
