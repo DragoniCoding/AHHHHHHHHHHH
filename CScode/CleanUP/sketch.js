5471412e80ece293b7b32ed5791882eb19f8ea6b
DragoniCoding <126823174+DragoniCoding@users.noreply.github.com>
2023-03-28 15:25:19 -0600

    new demo

--- CScode/CleanUP/sketch.js
+++ CScode/CleanUP/sketch.js
@@ -9,6 +9,7 @@ let grid;
 
 const ROWS = 25;
 const COLS = 25;
+let dragSpeed = 9;
 let cellSize;
 let pX = 0;
 let pY = 0;
@@ -48,9 +49,35 @@ function keyTyped() {
   if (key === "a") { //move left
     moveP(-1, 0);
   }
+  if (key === " ") { //move left
+    console.log(mouseX, mouseY);
+  }
+  if (key === "e") {
+    grid = createEmptyGrid(ROWS, COLS);
+  }
 }
 
 function moveP(x, y) {
+  //sanity check for edge cases
+  if (pX + x >= 0 && pX + x < COLS &&
+    pY + y >= 0 && pY + y < ROWS) {
+  
+    //check if going to hit wall
+    if (grid[pY+y][pX+x] === 1) {
+      let tempX = pX;
+      let tempY = pY;
+  
+      pX += x;
+      pY += y;
+  
+      //update grid
+      grid[pY][pX] = 9;
+      grid[tempY][tempX] = 0;
+    }
+  }
+}
+
+function moveEnemy(x, y) {
   //sanity check for edge cases
   if (pX + x >= 0 && pX + x < COLS &&
     pY + y >= 0 && pY + y < ROWS) {
@@ -64,9 +91,25 @@ function moveP(x, y) {
       pY += y;
   
       //update grid
-      grid[pY][pX] = 9;
+      grid[pY][pX] = 5;
       grid[tempY][tempX] = 1;
     }
+  }
+}
+
+function enemyMove() {
+  let rand = random(100);
+  if (rand <= 25) { //move down
+    moveP(0, 1);
+  }
+  else if (rand <= 50) { //move up
+    moveP(0, -1);
+  }
+  else if (rand <= 75) { //move right
+    moveP(1, 0);
+  }
+  else if (rand <= 100) { //move left
+    moveP(-1, 0);
   }
 }
 
@@ -83,6 +126,20 @@ function mousePressed() {
   }
 }
 
+function mouseDragged() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  if (grid[y][x] === 0 && frameCount % dragSpeed === 0) {
+    grid[y][x] = 1;
+  }
+
+  else if (grid[y][x] === 1 && frameCount % dragSpeed === 0) {
+    grid[y][x] = 0;
+  }
+}
+
+
 function displayGrid() {
   for (let y = 0; y < ROWS; y++) {
     for (let x = 0; x < COLS; x++) {
@@ -94,6 +151,9 @@ function displayGrid() {
       }
       else if (grid[y][x] === 9) {
         fill("green");
+      }
+      else if (grid[y][x] === 5) {
+        fill("red");
       }
       rect(x * cellSize, y * cellSize, cellSize, cellSize);
     }
